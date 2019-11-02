4de1f7ba8ff491bb8ac8752f340c73bcc02be006
Hamza <hamza.dahmoun7@gmail.com>
2019-11-02 01:18:09 +0100

    writeTens() & oneNormalTens_toWord()

--- Numbers to Words.js
+++ Numbers to Words.js
@@ -30,57 +30,58 @@ console.log(numberToArrray(input));
 console.log(addUpToTrillionsText(numberToArrray(input)));
 console.log(writeOnes(addUpToTrillionsText(numberToArrray(input))));
 console.log(writeHundreds(writeOnes(addUpToTrillionsText(numberToArrray(input)))));
+console.log(writeTens(writeHundreds(writeOnes(addUpToTrillionsText(numberToArrray(input))))));
 /* THIS SHOULD BE OUR FUNCTION */
 //006452698012
 
-function numberToArrray(number){
+function numberToArrray(number) {
     //this function will do the following:
     //006452698012 --> [0, 0, 6] [4,5, 2] [6, 9, 8] [0, 1, 2]
     let myBigArray = [];
     let mySmallArray = [];
-    for(let i=0; i<number.length; i++){
+    for (let i = 0; i < number.length; i++) {
         mySmallArray.push(number[i]);
-        if(mySmallArray.length == 3){
+        if (mySmallArray.length == 3) {
             //lets puh this small array to the big array
             myBigArray.push(mySmallArray);
             mySmallArray = [];
         }
-        else if(i == number.length-1){
+        else if (i == number.length - 1) {
             //so this is the last character in the number, we'll add the character zero '0' to remaining positions in small array
             //and then push it to the big array
-            do{
+            do {
                 mySmallArray.push("0");
-            }while(mySmallArray<3)            
+            } while (mySmallArray < 3)
         }
     }
     return myBigArray;
 }
 
-function addUpToTrillionsText(arrayOfArrays){
+function addUpToTrillionsText(arrayOfArrays) {
     //this function does the following:
     //[0, 0, 6] [4,5, 2] [6, 9, 8] [0, 1, 2] -------> [0, 0, 6, "Billions"] [4, 5, 2, "Millions"] [6, 9, 8, "Thousands"] [0, 1, 2, ""]
     let range = ["", "Thousands", "Millions", "Billions", "Trillions"];
     let rangeIndex = 0;
-    for(let i = arrayOfArrays.length-1; i>=0; i--){
+    for (let i = arrayOfArrays.length - 1; i >= 0; i--) {
         arrayOfArrays[i].push(range[rangeIndex]);
         rangeIndex = rangeIndex + 1;
     }
     return arrayOfArrays;
 }
 
-function writeOnes(arrayOfArrays){
+function writeOnes(arrayOfArrays) {
     //this function does the following:
     //[0, 0, 6, "Billions"] [4, 5, 2, "Millions"] [6, 9, 8, "Thousands"] [0, 1, 2, ""]
     // ------> [0, 0, "six", "Billions"] [4, 5, "two", "Millions"] [6, 9, "eight", "Thousands"] [0, 1, 2, ""]
-    for(let i =0; i<arrayOfArrays.length; i++){
-        if(arrayOfArrays[i][1] != "1"){
+    for (let i = 0; i < arrayOfArrays.length; i++) {
+        if (arrayOfArrays[i][1] != "1") {
             //so it won't be a teen number
             arrayOfArrays[i][2] = oneNumber_toWord(arrayOfArrays[i][2]);
         }
     }
     return arrayOfArrays;
 }
-function oneNumber_toWord(character){
+function oneNumber_toWord(character) {
     switch (character) {
         case "0":
             return "";
@@ -115,21 +116,62 @@ function oneNumber_toWord(character){
     }
 }
 
-function writeTens(){
-
-}
-
-function writeHundreds(arrayOfArrays){
+function writeHundreds(arrayOfArrays) {
     //this function does the following:
     //[0, 0, "six", "Billions"] [4, 5, "two", "Millions"] [6, 9, "eight", "Thousands"] [0, 1, 2, ""]
     // -------> ["", 0, "six", "Billions"] ["four", 5, "two", "Millions"] ["six", 9, "eight", "Thousands"] ["", 1, 2, ""]
-    for(let i =0; i<arrayOfArrays.length; i++){
+    for (let i = 0; i < arrayOfArrays.length; i++) {
         arrayOfArrays[i][0] = oneNumber_toWord(arrayOfArrays[i][0]);
     }
     return arrayOfArrays;
 }
 
-function textArray_toWords(){
+function writeTens(arrayOfArrays) {
+    //this function does the following:
+    //["", 0, "six", "Billions"] ["four", 5, "two", "Millions"] ["six", 9, "eight", "Thousands"] ["", 1, 2, ""]
+    // --------> ["", "", "six", "Billions"] ["four", "fifty", "two", "Millions"] ["six", "ninety", "eight", "Thousands"] ["", "twelve", ""]
+    for (let i = 0; i < arrayOfArrays.length; i++) {
+        if (arrayOfArrays[i][1] != "1") {
+            //so it won't be a teen number
+            arrayOfArrays[i][1] = oneNormalTens_toWord(arrayOfArrays[i][1]);
+        }
+    }
+    return arrayOfArrays;
+}
+function oneNormalTens_toWord(character) {
+    //this  number return tens in words (except tens smaller than 20)
+    switch (character) {
+        case "0":
+            return "";
+            break;
+        case "2":
+            return "Twenty";
+            break;
+        case "3":
+            return "Thirty";
+            break;
+        case "4":
+            return "Fourty";
+            break;
+        case "5":
+            return "Fifty";
+            break;
+        case "6":
+            return "Sixty";
+            break;
+        case "7":
+            return "Seventy";
+            break;
+        case "8":
+            return "Eighty";
+            break;
+        case "9":
+            return "Ninety";
+            break;
+    }
+}
+
+function textArray_toWords() {
 
 }
 
@@ -148,7 +190,7 @@ function getRealLength(number) {
                 leftSide_zeroCounter = leftSide_zeroCounter + 1;
             }
         }
-        else{
+        else {
             break;
         }
     }
@@ -248,13 +290,13 @@ function startConverting(number) {
         let columnIndex = getNumberVolumeIndex(number);
         let output = "";
         for (let i = leftSide_zeroCounter; i < number.length; i++) {
-            if(number[i] != "0"){
+            if (number[i] != "0") {
                 output = output + displayOnes_inWords(number[i]) + " " + myArray[columnIndex];
                 columnIndex++;
             }
-            else{
+            else {
                 columnIndex++;
-            }           
+            }
             console.log(columnIndex + output);
         }
         return output;
